e17a6e636e46413b030017e32306e0650f680e71
zzkluck <zzkluck@qq.com>
2024-05-24 15:52:33 +0800

    feat(scene.py): 添加了绘制敌人数据的功能.

--- scene.py
+++ scene.py
@@ -1,8 +1,8 @@
 from manim import *
 from pathlib import Path
-from fetch_data import fetch_char_data
+from fetch_data import fetch_char_data, fetch_enemy_data
 
-class ArkVis(Scene):
+class ArkVisChar(Scene):
     def construct(self):
         self.camera.background_color = "#ece6e2"
         x_range, y_range = [300, 1550, 100], [0, 1050, 100]
@@ -26,6 +26,36 @@ class ArkVis(Scene):
 
         char_data = fetch_char_data()
         for px, py, pz, image_path in char_data:
+            if not Path(image_path).exists(): continue
+            img = ImageMobject(image_path)
+            img.scale(0.1 * pz / pz)    # TODO: image size should be adjusted by pz
+            img.move_to(axes.c2p(px, py))
+            self.add(img)
+
+class ArkVisEnemy(Scene):
+    def construct(self):
+        self.camera.background_color = "#ece6e2"
+        x_range, y_range = [0, 3000, 100], [0, 3000, 100]
+        axes = Axes(
+            x_range=x_range,
+            y_range=y_range,
+            x_axis_config={
+                "numbers_to_include": np.arange(*x_range),
+            },
+            y_axis_config={
+                "numbers_to_include": np.arange(*y_range),
+            },
+            axis_config= {
+                'include_ticks': True,
+                'font_size': 16,
+            },
+            tips=False
+        )
+        axes.set_color(BLACK)
+        self.add(axes)
+
+        enemy_data = fetch_enemy_data(download_images=False)
+        for px, py, pz, image_path in enemy_data:
             if not Path(image_path).exists(): continue
             img = ImageMobject(image_path)
             img.scale(0.1 * pz / pz)    # TODO: image size should be adjusted by pz
